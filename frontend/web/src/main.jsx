9afc2ecc883acd0dfbf9da6f9cd888b3ad921311
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-11-11 02:07:13 -0800

    Update main.jsx

--- frontend/web/src/main.jsx
+++ frontend/web/src/main.jsx
@@ -2,13 +2,20 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+
 import Root from "./screens/root";
 import Console from "./screens/console";
+import Media from "./screens/media";
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     children: [
+      {
+        path: "/",
+        element: <Media />,
+      },
       {
         path: "/console",
         element: <Console />,
