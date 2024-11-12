fc81d962489e499c8162337be0c3ca9722e44329
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-11-12 03:02:53 -0800

    Update router.js

--- backend/helpers/router.js
+++ backend/helpers/router.js
@@ -4,10 +4,14 @@ The default export of this file is used by the express app as the main router
 
 The standards for REST API endpoints used in this router can be found here:
 https://restfulapi.net/resource-naming/
+
+The standards for HTTP response codes was found here:
+https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
 */
 
 import express from "express";
-import { selectMedia } from "../database/queries.js";
+import { selectMediaCollection, selectMedia } from "../database/queries.js";
+import { fetchPopularMovies } from "./tmdbLink.js";
 const router = express.Router();
 const v1 = express.Router();
 
@@ -17,7 +21,7 @@ router.get("/", (req, res) => {
 
 // #region v1 endpoints
 v1.get("/media", async (req, res) => {
-  const mediaCollection = await selectMedia(
+  const mediaCollection = await selectMediaCollection(
     req.query.offset,
     req.query.limit,
     req.query.order,
@@ -28,7 +32,17 @@ v1.get("/media", async (req, res) => {
 });
 
 v1.get("/media/:mediaId", async (req, res) => {
-  res.json();
+  const media = await selectMedia(req.params.mediaId);
+  if (media) {
+    res.json(media);
+  } 
+  /* 
+  sqlite3 will return undefined if the row is not found.
+  In this case send a Not Found response as the server was not able to find the resource
+  */
+  else {
+    res.sendStatus(404);
+  }
 });
 
 v1.get("/media/:mediaId/seasons", async (req, res) => {
@@ -49,6 +63,11 @@ v1.get(
     res.json();
   }
 );
+
+v1.get("/popular-movies", async (req, res) => {
+  const popular = await fetchPopularMovies();
+  res.json(popular);
+})
 // #endregion
 
 router.use("/v1", v1);
