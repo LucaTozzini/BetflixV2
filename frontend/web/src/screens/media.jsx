3283e7417bd5a0d978dbc74ca0b8ebda0eebf59a
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-11-12 03:03:19 -0800

    Update media.jsx

--- frontend/web/src/screens/media.jsx
+++ frontend/web/src/screens/media.jsx
@@ -1,20 +1,33 @@
 import { useEffect } from "react";
-import { MediaTable, MediaRow } from "../components/media-table";
-import Queries from "../hooks/queries";
-import styles from "../styles/media.module.css";
+import { useParams } from "react-router-dom";
+import useQueries from "../hooks/useQueries";
+import styles from "../styles/media.module.css"
+
 export default function Media() {
-  const latest = Queries();
+  const { mediaId } = useParams();
+  const media = useQueries()
+
+  /**
+   * Convert seconds to a readable time string
+   * @param {number} s
+   * @returns {string} 
+   */
+  function secondsToString(s) {
+    const hours = Math.floor(s / 3600)
+    const minutes = Math.floor((s % 3600) / 60)
+    const seconds = s % 60
+    return [hours ? `${hours}h` : null, minutes ? `${minutes}m` : null, !hours && !minutes ? `${seconds}s` : ""].filter(i => i).join(" "); 
+  }
+
   useEffect(() => {
-    latest.queryMedia(null, 30, "year", true, null);
-  }, []);
+    media.selectMedia(mediaId);
+  }, [])
 
   return (
     <div id="outlet" className={styles.container}>
-      <MediaTable title={"Latest"}>
-        {latest.data.map((i, index) => (
-          <MediaRow key={index} title={i.title} type={i.type} year={i.year} />
-        ))}
-      </MediaTable>
+      <h1>{media.data?.title} <span>{media.data?.year}</span></h1>
+      <h3>{secondsToString(media.data?.duration)}</h3>
+      <span><button>Play</button><button>Link</button></span>
     </div>
   );
 }
