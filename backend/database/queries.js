d3e64c27c9cf7454c66098426d0208fde338e268
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-11-12 03:02:51 -0800

    Update queries.js

--- backend/database/queries.js
+++ backend/database/queries.js
@@ -1,10 +1,22 @@
+/**
+ * To use database, await dbPromise which will return a Database instance.
+ * dbPromise.then(db => ...)
+ *
+ * The API documentations for sqlite3 can be found here: https://github.com/TryGhost/node-sqlite3/wiki/API
+ */
+
 import dbPromise from "./connection.js";
 
 /**
- * @param {any} limit
+ * Retrieves a media collection based on the parameters given
+ * @param {number} offset
+ * @param {number} limit
+ * @param {string} order
+ * @param {boolean} desc
+ * @param {string} type
  * @return {Promise<[]>}
  */
-export function selectMedia(offset, limit, order, desc, type) {
+export function selectMediaCollection(offset, limit, order, desc, type) {
   // If no type is specified, assume all types are requested
   const typeA = type ? type : "movie";
   const typeB = type ? type : "show";
@@ -26,4 +38,19 @@ export function selectMedia(offset, limit, order, desc, type) {
       );
     });
   });
+}
+
+/**
+ * Retrieves a single media row with the given media_id
+ * @param {number} mediaId
+ * @returns
+ */
+export function selectMedia(mediaId) {
+  return new Promise((res, rej) =>
+    dbPromise.then((db) =>
+      db.get("SELECT * FROM media WHERE media_id = ?", [mediaId], (err, row) =>
+        err ? rej(err) : res(row)
+      )
+    )
+  );
 }
