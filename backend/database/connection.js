04e1044951f5cd8d07eda8d07ffbfa0155bd2c66
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-11-12 03:02:49 -0800

    Update connection.js

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,3 +1,10 @@
+/** 
+ * I don't know what the best way to share a Database instance is. I'm exporting a promise which resolves the instance.
+ * This should make it so that other files can await/then in order to use db only once it is initialized.
+ * 
+ * The API documentations for sqlite3 can be found here: https://github.com/TryGhost/node-sqlite3/wiki/API 
+ */
+
 import sqlite3 from "sqlite3";
 import dotenv from "dotenv";
 dotenv.config();
