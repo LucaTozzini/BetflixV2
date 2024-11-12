b72b6b95f96bf87871e0d87411c43c9829a04e05
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-11-12 03:03:02 -0800

    Update media-table.jsx

--- frontend/web/src/components/media-table.jsx
+++ frontend/web/src/components/media-table.jsx
@@ -1,13 +1,12 @@
 import styles from "../styles/media-table.module.css";
-import moreIcon from "../assets/more-vertical.svg"
+import { Link } from "react-router-dom";
 
 export function MediaTable({ title, children }) {
   return (
     <div className={styles.table}>
-      <h2>{title} <span>{children.length}</span></h2>
+      <h2>{title} <span>{children?.length ?? 0}</span></h2>
       <div>
         <div className={styles.row}>
-          <div></div>
           <div>Title</div>
           <div>Year</div>
           <div>Type</div>
@@ -18,15 +17,12 @@ export function MediaTable({ title, children }) {
   );
 }
 
-export function MediaRow({ title, year, type }) {
+export function MediaRow({ media_id, tmdb_id, title, year, type }) {
   return (
-    <a onClick={() => window.location.href = "/console"} className={styles.row}>
-      <button onClick={(e) => {e.stopPropagation() }}>
-        <img src={moreIcon}/>
-      </button>
-      <div>{title}</div>
-      <div>{year}</div>
-      <div>{type}</div>
-    </a>
+    <Link to={"/media/"+media_id} className={styles.row}>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.year}>{year ?? "-"}</div>
+      <div className={styles.type}>{type}</div>
+    </Link>
   );
 }
