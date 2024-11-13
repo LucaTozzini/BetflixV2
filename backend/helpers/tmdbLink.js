ff2926cd7f74c4b939da1b236cd5b8c7bff26c8b
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-11-13 02:46:59 -0800

    Update tmdbLink.js

--- backend/helpers/tmdbLink.js
+++ backend/helpers/tmdbLink.js
@@ -1,3 +1,9 @@
+/**
+ * Allow HTTP server to interact with the TMDb API through these functions
+ * Documentations for TMDBb API v3 are found here:
+ * https://developer.themoviedb.org/reference/intro/getting-started
+ */
+
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -5,7 +11,7 @@ const API_ADDRESS = "https://api.themoviedb.org/3";
 
 /**
  * Fetch the current popular movies on TMDb
- * @returns {Promise<json>}
+ * @returns {Promise}
  */
 export async function fetchPopularMovies() {
   try {
@@ -19,8 +25,32 @@ export async function fetchPopularMovies() {
     };
 
     const response = await fetch(url, options);
-    const {results} = await response.json();
+    const { results } = await response.json();
     return results;
+  } catch (err) {
+    throw err;
+  }
+}
+
+/**
+ * Fetch the details for a single movie
+ * @param {number} tmdbId 
+ * @returns {Promise}
+ */
+export async function fetchMovieDetails(tmdbId) {
+  try {
+    const url = `${API_ADDRESS}/movie/${tmdbId}`;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${process.env.TMDB_READ_TOKEN}`,
+      },
+    };
+
+    const response = await fetch(url, options);
+    const json = await response.json();
+    return json;
   } catch (err) {
     throw err;
   }
