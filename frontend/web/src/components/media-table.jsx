154c93c2aed8d5f2802dda18a5c335a995a1371c
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-11-13 23:40:35 -0800

    Update media-table.jsx

--- frontend/web/src/components/media-table.jsx
+++ frontend/web/src/components/media-table.jsx
@@ -1,28 +1,41 @@
 import styles from "../styles/media-table.module.css";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export function MediaTable({ title, children }) {
   return (
-    <div className={styles.table}>
-      <h2>{title} <span>{children?.length ?? 0}</span></h2>
-      <div>
-        <div className={styles.row}>
-          <div>Title</div>
-          <div>Year</div>
-          <div>Type</div>
-        </div>
-        {children}
+    <div className={styles.container}>
+      <h2>
+        {title} <span>{children?.length ?? 0}</span>
+      </h2>
+      <div className={styles.wrap}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Year</th>
+              <th>Genres</th>
+              <th>Type</th>
+            </tr>
+          </thead>
+          <tbody>{children}</tbody>
+        </table>
       </div>
     </div>
   );
 }
 
-export function MediaRow({ mediaId, tmdbId, title, year, type }) {
+export function MediaRow({ mediaId, tmdbId, title, year, genres, type }) {
+  const navigate = useNavigate();
   return (
-    <Link to={mediaId ? `/media/${mediaId}` : `/external/${tmdbId}`} className={styles.row}>
-      <div className={styles.title}>{title}</div>
-      <div className={styles.year}>{year ?? "-"}</div>
-      <div className={styles.type}>{type}</div>
-    </Link>
+    <tr
+      onClick={() =>
+        navigate(mediaId ? `/media/${mediaId}` : `/external/${tmdbId}`)
+      }
+    >
+      <td data-cell="title">{title ?? "-"}</td>
+      <td data-cell="year">{year ?? "-"}</td>
+      <td data-cell="genres">{genres ?? "-"}</td>
+      <td data-cell="type">{type ?? "-"}</td>
+    </tr>
   );
 }
