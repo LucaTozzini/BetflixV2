21d7af066f46d8be154a315c48778d4aca83de2c
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-11-13 23:41:00 -0800

    Update media.jsx

--- frontend/web/src/screens/media.jsx
+++ frontend/web/src/screens/media.jsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useQueries from "../hooks/useQueries";
+import { TorrentsTable, TorrentRow } from "../components/torrents-table";
 import styles from "../styles/media.module.css";
 
-const TMDB_IMG_BASE = "https://image.tmdb.org/t/p/original"
+const TMDB_IMG_BASE = "https://image.tmdb.org/t/p/original";
 
 /**
  * Convert seconds to a readable time string
@@ -11,6 +12,9 @@ const TMDB_IMG_BASE = "https://image.tmdb.org/t/p/original"
  * @returns {string}
  */
 function secondsToString(s) {
+  if (isNaN(s)) {
+    return "";
+  }
   const hours = Math.floor(s / 3600);
   const minutes = Math.floor((s % 3600) / 60);
   const seconds = s % 60;
@@ -23,6 +27,32 @@ function secondsToString(s) {
     .join(" ");
 }
 
+const MediaInfo = ({ title, year, duration, buttons, genres, overview }) => {
+  const navigate = useNavigate();
+  const { mediaId } = useParams();
+
+  return (
+    <div className={styles.info}>
+      <h1>
+        {title} <span>{year}</span>
+      </h1>
+      <h3>
+        {secondsToString(duration)}{" "}
+        <span className={styles.genres}>
+          {genres ? `| ${genres.join(", ")}` : ""}
+        </span>
+      </h3>
+      {buttons && (
+        <span>
+          <button onClick={() => navigate(`/video/${mediaId}`)}>Play</button>
+          <button>Link</button>
+        </span>
+      )}
+      <p>{overview}</p>
+    </div>
+  );
+};
+
 export function LocalMedia() {
   const { mediaId } = useParams();
   const media = useQueries();
@@ -34,41 +64,60 @@ export function LocalMedia() {
   return (
     <div id="outlet" className={styles.container}>
       <div className={styles.wrap}>
-        <h1>
-          {media.data?.title} <span>{media.data?.year}</span>
-        </h1>
-        <h3>{secondsToString(media.data?.duration)}</h3>
-        <span>
-          <button>Play</button>
-          <button>Link</button>
-        </span>
+        <MediaInfo
+          title={media.data?.title}
+          year={media.data?.year}
+          duration={secondsToString(media.data?.duration)}
+          buttons={true}
+        />
       </div>
     </div>
   );
 }
 
 export function ExternalMedia() {
-  const { imdbId } = useParams();
+  const { tmdbId } = useParams();
   const media = useQueries();
+  const torrents = useQueries();
 
   useEffect(() => {
-    media.fetchMovieDetails(imdbId);
+    media.fetchMovieDetails(tmdbId);
   }, []);
 
   useEffect(() => {
-    console.log(media.data)
-  }, [media.data])
+    if (media.data?.imdb_id) {
+      torrents.fetchMovieTorrents(media.data.imdb_id);
+      console.log(media.data);
+    }
+  }, [media.data]);
 
   return (
     <div id="outlet" className={styles.container}>
-      {media.data?.backdrop_path && <img src={TMDB_IMG_BASE + media.data.backdrop_path} alt="backdrop" />}
+      {media.data?.backdrop_path && (
+        <img src={TMDB_IMG_BASE + media.data.backdrop_path} alt="backdrop" />
+      )}
       <div className={styles.wrap}>
-      <h1>
-        {media.data?.title} <span>{media.data?.release_date?.split("-")[0]}</span>
-      </h1>
-      <h3>{secondsToString(media.data?.runtime * 60)}</h3>
-      <p>{media.data?.overview}</p>
-
+        <MediaInfo
+          title={media.data?.title}
+          year={media.data?.release_date?.split("-")[0]}
+          duration={media.data?.runtime * 60}
+          overview={media.data?.overview}
+          genres={media.data?.genres?.map((i) => i.name)}
+        />
+        <TorrentsTable>
+          {torrents.data?.map((i, index) => (
+            <TorrentRow
+              key={index}
+              quality={i.quality}
+              type={i.type}
+              seeds={i.seeds}
+              peers={i.peers}
+              size={i.size}
+              codec={i.video_codec}
+              uploaded={i.date_uploaded.split(" ")[0]}
+            />
+          ))}
+        </TorrentsTable>
       </div>
     </div>
   );
