2ec61a235da56cda5cd34f9b95ae58d716ba858b
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-11-17 23:14:07 -0800

    Update video.jsx

--- frontend/web/src/screens/video.jsx
+++ frontend/web/src/screens/video.jsx
@@ -1,11 +1,29 @@
 import { useParams } from "react-router-dom";
 import styles from "../styles/video.module.css";
+import { useEffect, useLayoutEffect } from "react";
+import useQueries from "../hooks/useQueries";
 
 export default function Video() {
+  const media = useQueries()
   const {mediaId} = useParams();
+  useLayoutEffect(() => {
+    document.title = "Streaming | Betflix"
+  }, [])
+
+  useEffect(() => {
+    media.selectMedia(mediaId);
+  }, [])
+
+  useEffect(() => {
+    if(media.data) {
+      console.log(media.data);
+      document.title = `Streaming ${media.data.title}`
+    }
+  }, [media.data])
+
   return (
-    <div id="outlet">
-      <video controls src={`http://localhost:5340/v1/stream/${mediaId}`}/>
+    <div id="outlet" className={styles.container}>
+      <video className={styles.video} controls src={`http://localhost:5340/stream/${mediaId}`}/>
     </div>
   );
 }
