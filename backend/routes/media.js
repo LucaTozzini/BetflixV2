180a80102face365e0120760ed88f890a640a15e
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-12-02 19:42:10 -0800

    Update media.js

--- backend/routes/media.js
+++ backend/routes/media.js
@@ -5,10 +5,13 @@ const mediaRouter = express.Router();
 mediaRouter.get("/", mediaController.mediaCollection);
 mediaRouter.get("/:mediaId", mediaController.mediaDetails);
 mediaRouter.get("/:mediaId/seasons", mediaController.seasons);
-mediaRouter.get("/:mediaId/seasons/:seasonNum", mediaController.season);
 mediaRouter.get(
   "/:mediaId/seasons/s:seasonNum-e:episodeNum",
   mediaController.episode
 );
+
+// needs to come AFTER episode or else episode request will be interpreted as season 
+mediaRouter.get("/:mediaId/seasons/:seasonNum", mediaController.season);
+
 
 export default mediaRouter;
