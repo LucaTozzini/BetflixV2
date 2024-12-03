bcd9c935257e855b0c2234616295f8080e10bf5e
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-12-02 19:42:01 -0800

    Update external.js

--- backend/routes/external.js
+++ backend/routes/external.js
@@ -2,12 +2,9 @@ import externalController from "../controllers/externalController.js";
 import express from "express";
 const externalRouter = express.Router();
 
-externalRouter.get(
-  "/popular-movies",
-  externalController.popularMovies
-);
+externalRouter.get("/popular-movies", externalController.popularMovies);
 externalRouter.get("/movies", externalController.movies);
 externalRouter.get("/movies/:tmdbId", externalController.movieDetails);
-externalRouter.get("/torrents/:imdbId", externalController.torrents);
+externalRouter.get("/shows", externalController.shows);
 
 export default externalRouter;
