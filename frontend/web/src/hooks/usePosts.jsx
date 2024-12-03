22723251918f20218dcbc95c0b73f459e10cc1a9
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-12-02 19:43:43 -0800

    Update usePosts.jsx

--- frontend/web/src/hooks/usePosts.jsx
+++ frontend/web/src/hooks/usePosts.jsx
@@ -3,12 +3,12 @@ const SERVER = "localhost:5340";
 export default function usePosts() {
   async function postTorrent(fileURL) {
     const response = await fetch(`http://${SERVER}/torrents?fileURL=${fileURL}`, {method: "POST"})
-    return response.status
+    return response
   }
 
-  async function postLink(mediaId, tmdbId) {
-    const response = await fetch(`http://${SERVER}/link?mediaId=${mediaId}&tmdbId=${tmdbId}`)
-    return response.status
+  async function postLink(mediaId, tmdbId, type) {
+    const response = await fetch(`http://${SERVER}/link?mediaId=${mediaId}&tmdbId=${tmdbId}&type=${type}`, {method: "POST"})
+    return response
   }
   
   return {postTorrent, postLink}
