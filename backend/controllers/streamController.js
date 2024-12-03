9f114086077caf457206a3c620e545be465cd259
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-12-03 03:26:22 -0800

    Update streamController.js

--- backend/controllers/streamController.js
+++ backend/controllers/streamController.js
@@ -1,14 +1,39 @@
 import fs from "fs";
-import { selectMedia } from "../database/reads.js";
-import { mimeType, CHUNK_SIZE } from "../helpers/stream.js";
+import path from "path";
+import { selectMedia, selectEpisode } from "../database/reads.js";
 
-async function streamMovie(req, res) {
-  const media = await selectMedia(req.params.mediaId);
-  if (!media || media.type === "show") {
-    res.sendStatus(404);
-    return;
-  }
-  const videoSize = fs.statSync(media.path).size;
+const CHUNK_SIZE = 10 ** 6;
+
+const MIME = {
+  ".aac": "audio/aac",
+  ".abw": "application/x-abiword",
+  ".arc": "application/x-freearc",
+  ".avif": "image/avif",
+  ".avi": "video/x-msvideo",
+  ".mp4": "video/mp4",
+  ".m4v": "video/mp4",
+  ".mpeg": "video/mpeg",
+  ".ogv": "video/ogg",
+  ".ts": "video/mp2t",
+  ".webm": "video/webm",
+};
+
+/**
+ * @param {string} videoPath 
+ * @returns {string} 
+ */
+function mimeType(videoPath) {
+  const ext = path.extname(videoPath);
+  return MIME[ext] ?? "video";
+}
+
+/**
+ * @param {import('express').Request} res
+ * @param {import('express').Response} res
+ * @param {string} videoPath
+ */
+function pipeStream(req, res, videoPath) {
+  const videoSize = fs.statSync(videoPath).size;
 
   // header.range is expected to be like -> 'bytes=0-'
   const range = req.headers.range?.match(/bytes=(?<start>[0-9]+)-.*/);
@@ -19,15 +44,59 @@ async function streamMovie(req, res) {
     "Content-Range": `bytes ${start}-${end}/${videoSize}`,
     "Accept-Ranges": "bytes",
     "Content-Length": end - start + 1, // add 1 because bytes are zero-indexed
-    "Content-Type": mimeType(media.path) ?? "",
+    "Content-Type": mimeType(videoPath) ?? "",
   });
 
-  const stream = fs.createReadStream(media.path, { start, end });
+  const stream = fs.createReadStream(videoPath, { start, end });
   stream.pipe(res);
 }
 
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
+async function streamMovie(req, res) {
+  const media = await selectMedia(req.params.mediaId);
+  if (!media) {
+    res.sendStatus(404);
+    return;
+  }
+  if (media.type === "show") {
+    res.sendStatus(400);
+    return;
+  }
+
+  pipeStream(req, res, media.path);
+}
+
+/**
+ * @param {import('express').Request} req 
+ * @param {import('express').Response} res 
+ * @returns 
+ */
 async function streamEpisode(req, res) {
-  res.send(`Season ${req.params.seasonNum} Episode ${req.params.episodeNum}`);
+  const media = await selectMedia(req.params.mediaId);
+  if (!media) {
+    res.sendStatus(404);
+    return;
+  }
+  if (media.type === "movie") {
+    res.sendStatus(400);
+    return;
+  }
+
+  const episode = await selectEpisode(
+    media.media_id,
+    req.params.seasonNum,
+    req.params.episodeNum
+  );
+
+  if (!episode) {
+    res.sendStatus(404);
+    return;
+  }
+
+  pipeStream(req, res, episode.path);
 }
 
 export default { streamMovie, streamEpisode };
