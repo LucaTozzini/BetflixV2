dddae73a72ab6bee741dba41d707af77be2edd6d
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-12-02 19:41:52 -0800

    Create writes.js

---
+++ backend/database/writes.js
@@ -0,0 +1,69 @@
+/*
+To use database, await dbPromise which will return a Database instance.
+dbPromise.then(db => ...)
+
+The API documentations for sqlite3 can be found here: https://github.com/TryGhost/node-sqlite3/wiki/API
+*/
+
+import dbPromise from "./connection.js";
+
+/**
+ * Insert local to external link
+ * @param {number} mediaId
+ * @param {number} tmdbId
+ * @param {string} poster
+ * @param {string} backdrop
+ * @param {string} overview
+ * @param {string} genres
+ * @returns {Promise<object>}
+ */
+export function insertLink(
+  mediaId,
+  tmdbId,
+  title,
+  poster,
+  backdrop,
+  overview,
+  genres
+) {
+  return new Promise((res, rej) =>
+    dbPromise.then((db) =>
+      db.run(
+        "INSERT INTO link (media_id, tmdb_id, title, poster, backdrop, overview, genres) VALUES (?,?,?,?,?,?,?)",
+        [mediaId, tmdbId, title, poster, backdrop, overview, genres],
+        (err) => (err ? rej(err) : res())
+      )
+    )
+  );
+}
+
+/**
+ * INSERT a show into the media TABLE
+ * @param {string} path absolute path of show folder
+ * @param {string} title
+ * @param {number} year
+ * @returns {Promise}
+ */
+export function insertShow(path, title, year) {
+  return new Promise((res, rej) =>
+    dbPromise.then((db) =>
+      db.run(
+        `INSERT INTO media(path, title, year, type) VALUES (?,?,?,"show")`,
+        [path, title, year],
+        (err) => (err ? rej(err) : res())
+      )
+    )
+  );
+}
+
+export function insertEpisode(mediaId, path, seasonNum, episodeNum, duration) {
+  return new Promise((res, rej) =>
+    dbPromise.then((db) =>
+      db.run(
+        "INSERT INTO episodes(media_id, path, season_num, episode_num, duration) VALUES (?,?,?,?,?)",
+        [mediaId, path, seasonNum, episodeNum, duration],
+        (err) => (err ? rej(err) : res())
+      )
+    )
+  );
+}
