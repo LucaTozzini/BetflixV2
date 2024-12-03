a0a394dc19592d4b0ea4949d3b5fc49f2265e4f2
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-12-02 19:41:56 -0800

    Update tmdbLink.js

--- backend/helpers/tmdbLink.js
+++ backend/helpers/tmdbLink.js
@@ -4,27 +4,137 @@
  * https://developer.themoviedb.org/reference/intro/getting-started
  */
 
+// #region JSDocs
+/**
+ * https://developer.themoviedb.org/reference/movie-popular-list
+ * @typedef {Object} listMovieItem
+ * @property {boolean} adult
+ * @property {string} backdrop_path
+ * @property {Array<number>} genres_ids
+ * @property {number} id
+ * @property {string} original_language
+ * @property {string} original_title
+ * @property {string} overview
+ * @property {number} popularity
+ * @property {string} poster_path
+ * @property {string} release_date
+ * @property {string} title
+ * @property {boolean} video
+ * @property {number} vote_average
+ * @property {number} vote_count
+ *
+ */
+
+/**
+ * @typedef {Object} listShowItem
+ * @property {boolean} adult
+ * @property {string} backdrop_path
+ * @property {Array<number>} genres_ids
+ * @property {number} id
+ * @property {string} original_country
+ * @property {string} original_language
+ * @property {string} original_name
+ * @property {string} overview
+ * @property {number} popularity
+ * @property {string} poster_path
+ * @property {string} first_air_date
+ * @property {string} name
+ * @property {number} vote_average
+ * @property {number} vote_count
+ */
+
+/**
+ * https://developer.themoviedb.org/reference/movie-details
+ * @typedef {Object} movieDetails
+ * @property {boolean} adult
+ * @property {string} backdrop_path
+ * @property {number} belongs_to_collection
+ * @property {number} budget
+ * @property {Array<{id: number, name: string}>} genres
+ * @property {string} homepage
+ * @property {number} id
+ * @property {string} imdb_id
+ * @property {string} original_language
+ * @property {string} original_title
+ * @property {string} overview
+ * @property {number} popularity
+ * @property {string} poster_path
+ * @property {Array<{id: number, logo_path: string, name: string, origin_country: string}>} production_companies
+ * @property {Array<{iso_3166_1: string, name: string}>} production_countries
+ * @property {string} release_date
+ * @property {number} revenue
+ * @property {number} runtime
+ * @property {Array<{english_name: string, iso_639_1: string, name: string}>} spoken_languages
+ * @property {string} status
+ * @property {string} tagline
+ * @property {string} title
+ * @property {boolean} video
+ * @property {number} vote_average
+ * @property {number} vote_count
+ */
+
+/**
+ * @typedef {Object} showDetails
+ * @property {boolean} adult
+ * @property {string} backdrop_path
+ * @property {Array<{id: number, credit_id: number, gender: number, profile_path: string}>} created_by
+ * @property {Array<number>} episode_run_time 
+ * @property {string} first_air_date
+ * @property {Array<{id: number, name: string}>} genres
+ * @property {string} homepage
+ * @property {number} id
+ * @property {boolean} in_production
+ * @property {Array<string>} languages
+ * @property {string} last_air_date
+ * @property {episodeDetails} last_episode_to_air
+ * @property {string} name
+ * @property {string} next_episode_to_air
+ * @property {Array<id: number, logo_path: string, name: string, origin_country: string>} networks
+ * @property {number} number_of_episodes
+ * @property {number} number_of_seasons
+ * @property {string} origin_country
+ * @property {string} original_language
+ * @property {string} original_name
+ * @property {string} overview
+ * @property {number} popularity
+ * @property {string} poster_path
+ * @property {Array<{id: number, logo_path: string, name: string, origin_country: string}>} production_companies
+ * @property {Array<{iso_3166_1: string, name: string}>} production_countries
+ * @property {Array<{air_date: string, episode_count: number, id: number, overview: string, poster_path: string, season_number: number, vote_average: number}>} seasons
+ * @property {Array<english_name: string, iso_639_1: string, name: string>} spoken_languages
+ * @property {string} status
+ * @property {string} tagline
+ * @property {string} type
+ * @property {number} vote_average
+ * @property {number} vote_count
+ * 
+ */
+
+/**
+ * @typedef {Object} episodeDetails
+ */
+// #endregion
+
 import dotenv from "dotenv";
 dotenv.config();
 
 const API_ADDRESS = "https://api.themoviedb.org/3";
+const GET_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.TMDB_READ_TOKEN}`,
+  },
+};
 
 /**
  * Fetch the current popular movies on TMDb
- * @returns {Promise}
+ * @returns {Promise<Array<listMovieItem>>}
  */
 export async function fetchPopularMovies() {
   try {
     const url = `${API_ADDRESS}/movie/popular?language=en-US&page=1`;
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${process.env.TMDB_READ_TOKEN}`,
-      },
-    };
-
-    const response = await fetch(url, options);
+    const response = await fetch(url, GET_OPTIONS);
     const { results } = await response.json();
     return results;
   } catch (err) {
@@ -35,50 +145,12 @@ export async function fetchPopularMovies() {
 /**
  * Fetch the details for a single movie
  * @param {number} tmdbId
- * @returns {Promise}
+ * @returns {Promise<movieDetails>}
  */
 export async function fetchMovieDetails(tmdbId) {
   try {
     const url = `${API_ADDRESS}/movie/${tmdbId}`;
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${process.env.TMDB_READ_TOKEN}`,
-      },
-    };
-
-    const response = await fetch(url, options);
-
-    /* Example:
-      {
-        "adult": false,
-        "backdrop_path": "/hZkgoQYus5vegHoetLkCJzb17zJ.jpg",
-        "belongs_to_collection": null,
-        "budget": 63000000,
-        "genres": [{ "id": 18, "name": "Drama"}, ...],
-        "homepage": "http://www.foxmovies.com/movies/fight-club",
-        "id": 550,
-        "imdb_id": "tt0137523",
-        "original_language": "en",
-        "original_title": "Fight Club",
-        "overview": "A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.",
-        "popularity": 61.416,
-        "poster_path": "/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
-        "production_companies": [ {"id": 508, "logo_path": "/7cxRWzi4LsVm4Utfpr1hfARNurT.png", "name": "Regency Enterprises", "origin_country": "US"}, ... ],
-        "production_countries": [ {"iso_3166_1": "US", "name": "United States of America"}, ... ],
-        "release_date": "1999-10-15",
-        "revenue": 100853753,
-        "runtime": 139,
-        "spoken_languages": [ {"english_name": "English", "iso_639_1": "en", "name": "English" }, ... ],
-        "status": "Released",
-        "tagline": "Mischief. Mayhem. Soap.",
-        "title": "Fight Club",
-        "video": false,
-        "vote_average": 8.433,
-        "vote_count": 26280
-      }
-    */
+    const response = await fetch(url, GET_OPTIONS);
     const json = await response.json();
     return json;
   } catch (err) {
@@ -87,24 +159,49 @@ export async function fetchMovieDetails(tmdbId) {
 }
 
 /**
+ * @param {number} tmdbId 
+ * @returns {Promise<showDetails>}
+ */
+export async function fetchShowDetails(tmdbId) {
+  try {
+    const url = `${API_ADDRESS}/tv/${tmdbId}`
+    const response = await fetch(url, GET_OPTIONS);
+    const json = await response.json()
+    return json;
+  } catch(err) {
+    throw err
+  }
+}
+
+/**
  * Serach for movies in TMDb
  * Note: query is required
  * @param {string} query
- * @param {int} year
+ * @param {number} year
+ * @returns {Promise<Array<listMovieItem>>}
  */
 export async function fetchMovies(query, year) {
   try {
-    const url = `${API_ADDRESS}/search/movie?query=${query}${
-      year ? `&year=${year}` : ""
-    }`;
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${process.env.TMDB_READ_TOKEN}`,
-      },
-    };
-    const response = await fetch(url, options);
+    let url = `${API_ADDRESS}/search/movie?query=${query}`;
+    if (year) url += `&year=${year}`;
+    const response = await fetch(url, GET_OPTIONS);
+    const json = await response.json();
+    return json.results;
+  } catch (err) {
+    throw err;
+  }
+}
+
+/**
+ * @param {string} query
+ * @param {number} year optional
+ * @returns {Promise<Array<listShowItem>>}
+ */
+export async function fetchShows(query, year) {
+  try {
+    let url = `${API_ADDRESS}/search/tv?query=${query}`;
+    if (year) url += `&year=${year}`;
+    const response = await fetch(url, GET_OPTIONS);
     const json = await response.json();
     return json.results;
   } catch (err) {
