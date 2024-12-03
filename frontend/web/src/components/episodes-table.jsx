0429734ecd8a1204749abef7fe82b6d6cfc49678
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-12-02 19:46:31 -0800

    Create episodes-table.jsx

---
+++ frontend/web/src/components/episodes-table.jsx
@@ -0,0 +1,51 @@
+import styles from "../styles/episodes-table.module.css";
+import { useNavigate } from "react-router-dom";
+
+/**
+ * Convert seconds to a readable time string
+ * @param {number} s
+ * @returns {string}
+ */
+function secondsToString(s) {
+  if (isNaN(s)) {
+    return "";
+  }
+  const hours = Math.floor(s / 3600);
+  const minutes = Math.floor((s % 3600) / 60);
+  const seconds = s % 60;
+  return [
+    hours ? `${hours}h` : null,
+    minutes ? `${minutes}m` : null,
+    !hours && !minutes ? `${seconds}s` : "",
+  ]
+    .filter((i) => i)
+    .join(" ");
+}
+
+export function EpisodesTable({ children }) {
+  return (
+    <div className={styles.wrap}>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>Season</th>
+            <th>Episode</th>
+            <th>Duration</th>
+          </tr>
+        </thead>
+        <tbody>{children}</tbody>
+      </table>
+    </div>
+  );
+}
+
+export function EpisodeRow({ mediaId, seasonNum, episodeNum, duration }) {
+  const navigate = useNavigate();
+  return (
+    <tr onClick={() => navigate(`/video/${mediaId}`)}>
+      <td data-cell="seasonNum">{seasonNum}</td>
+      <td data-cell="episodeNum">{episodeNum}</td>
+      <td data-cell="duration">{secondsToString(duration)}</td>
+    </tr>
+  );
+}
