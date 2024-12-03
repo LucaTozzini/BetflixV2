1fc528783b1eac33ebd7f3358f393e6b5c8f1a2e
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-12-02 19:42:54 -0800

    Update root.jsx

--- frontend/web/src/screens/root.jsx
+++ frontend/web/src/screens/root.jsx
@@ -1,6 +1,26 @@
+import { useLayoutEffect } from "react";
 import Navbar from "../components/navbar";
 import { Outlet } from "react-router-dom";
 export default function Root() {
+  useLayoutEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      document.getElementById("root").classList.add("dark");
+    }
+
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", (event) => {
+        console.log(event);
+        // Dark
+        if (event.matches) {
+          document.getElementById("root").classList.add("dark");
+        }
+        // Light
+        else {
+          document.getElementById("root").classList.remove("dark");
+        }
+      });
+  }, []);
   return (
     <>
       <Navbar />
