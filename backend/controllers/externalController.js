83759a37a8e0476b1ce9b27d5f6e1bfe127f52ff
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-12-05 03:43:58 -0800

    Update externalController.js

--- backend/controllers/externalController.js
+++ backend/controllers/externalController.js
@@ -3,6 +3,7 @@ import {
   fetchMovies,
   fetchMovieDetails,
   fetchShows,
+  fetchShowDetails,
 } from "../helpers/tmdbLink.js";
 
 async function popularMovies(req, res) {
@@ -29,15 +30,27 @@ async function shows(req, res) {
 }
 
 async function movieDetails(req, res) {
-  const details = await fetchMovieDetails(req.params.tmdbId);
-  res.json(details);
+  if (isNaN(req.params.tmdbId)) {
+    res.sendStatus(400);
+    return;
+  }
+  const data = await fetchMovieDetails(req.params.tmdbId);
+  res.json(data);
 }
 
-
+async function showDetails(req, res) {
+  if (isNaN(req.params.tmdbId)) {
+    res.sendStatus(400);
+    return;
+  }
+  const data = await fetchShowDetails(req.params.tmdbId);
+  res.json(data);
+}
 
 export default {
   popularMovies,
   movies,
   shows,
   movieDetails,
+  showDetails
 };
