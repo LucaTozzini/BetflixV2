2ca40bd5be6922fbc84818af9b30b878aadd0f43
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-12-10 00:38:17 -0800

    Update [mediaId].jsx

--- frontend/mobile/app/stream/[mediaId].jsx
+++ frontend/mobile/app/stream/[mediaId].jsx
@@ -1,24 +1,30 @@
-import { useEvent } from 'expo';
+import { useEvent } from "expo";
 import { useLocalSearchParams } from "expo-router";
 import { useVideoPlayer, VideoView } from "expo-video";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { StyleSheet } from "react-native";
 import { View } from "react-native";
 const SERVER = "192.168.1.76:5340";
 
 export default () => {
-  const { mediaId } = useLocalSearchParams();
+  const params = useLocalSearchParams();
   const player = useVideoPlayer(
-    `http://${SERVER}/stream/${mediaId}`,
+    // If params include s (seasonNum) and e (episodeNum), assume tv show and query episode
+    `http://${SERVER}/stream/${params.mediaId}${
+      params.s && params.e ? `/s${params.s}-e${params.e}` : ""
+    }`,
     (player) => {
       player.loop = false;
       player.play();
     }
   );
-  const { status, error } = useEvent(player, 'statusChange', { status: player.status });
+  const { status, error } = useEvent(player, "statusChange", {
+    status: player.status,
+  });
   useEffect(() => {
-    console.log(error, status)
-  }, [status, error])
+    // console.log(error, status)
+  }, [status, error]);
+
   return (
     <View style={styles.container}>
       <VideoView player={player} style={styles.videoView} />
@@ -29,9 +35,9 @@ export default () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "black"
+    backgroundColor: "black",
   },
   videoView: {
-    flex: 1
-  }
-})
+    flex: 1,
+  },
+});
