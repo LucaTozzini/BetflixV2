871671226e171491137a7429e5038a5a8e9de3b3
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-12-10 00:38:25 -0800

    Update useTheme.jsx

--- frontend/mobile/hooks/useTheme.jsx
+++ frontend/mobile/hooks/useTheme.jsx
@@ -1,29 +1,40 @@
 import { useState } from "react";
 
 export default function useTheme() {
-
   const [color, setColor] = useState("black");
   const [colorDim, setColorDim] = useState("grey");
   const [backgroundColor, setBackgroundColor] = useState("white");
   const [highlightColor, setHighlightColor] = useState("lightblue");
-  
+  const [statusBarStyle, setStatusBarStyle] = useState("light");
+
   const [h1Size, setH1Size] = useState(20);
   const [h2Size, setH2Size] = useState(17);
 
-  function setLightTheme(){
-    setColor("black")
-    setColorDim("grey")
-    setBackgroundColor("white")
-    setHighlightColor("lightblue")
+  function setLightTheme() {
+    setStatusBarStyle("dark");
+    setColor("black");
+    setColorDim("grey");
+    setBackgroundColor("white");
+    setHighlightColor("lightblue");
   }
 
-  function setDarkTheme(){
-    setColor("white")
-    setColorDim("grey")
-    setBackgroundColor("rgb(20,20,20)")
-    setHighlightColor("lightblue")
+  function setDarkTheme() {
+    setStatusBarStyle("light");
+    setColor("white");
+    setColorDim("grey");
+    setBackgroundColor("rgb(20,20,20)");
+    setHighlightColor("rgb(120,80,98)");
   }
-  
 
-  return {color, colorDim, h1Size, h2Size, backgroundColor, highlightColor, setLightTheme, setDarkTheme}
+  return {
+    color,
+    colorDim,
+    h1Size,
+    h2Size,
+    statusBarStyle,
+    backgroundColor,
+    highlightColor,
+    setLightTheme,
+    setDarkTheme,
+  };
 }
