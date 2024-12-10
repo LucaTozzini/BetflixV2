53697b854870dbb91323c668aff183e6b13c7acf
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-12-10 00:38:10 -0800

    Update _layout.jsx

--- frontend/mobile/app/(tabs)/_layout.jsx
+++ frontend/mobile/app/(tabs)/_layout.jsx
@@ -1,5 +1,11 @@
 import { Tabs } from "expo-router";
-import TabBar from "../../components/tabBar"
+import TabBar from "../../components/tabBar";
+
 export default () => {
-  return <Tabs screenOptions={{headerShown: false}} tabBar={props => <TabBar {...props}/>}/>;
+  return (
+    <Tabs
+      screenOptions={{ headerShown: false }}
+      tabBar={(props) => <TabBar {...props} />}
+    />
+  );
 };
