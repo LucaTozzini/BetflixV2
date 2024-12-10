58ec744e213af38120ee4e23442ec81df48b6f56
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-12-10 00:38:23 -0800

    Update secToString.jsx

--- frontend/mobile/helpers/secToString.jsx
+++ frontend/mobile/helpers/secToString.jsx
@@ -1,13 +1,14 @@
 /**
  * Convert seconds to human-readable string
- * @param {number} sec 
+ * @param {number} sec
  * @returns {string}
  */
-export default function secToString(sec) {
+export default function secToString(sec, includeSec) {
   const hours = Math.floor(sec / 3600);
   const minutes = Math.floor((sec % 3600) / 60);
   let string = [];
   if (hours) string.push(hours + "h");
   if (minutes) string.push(minutes + "m");
+  if (includeSec) string.push(Math.floor(sec % 60) + "s");
   return string.join(" ");
 }
