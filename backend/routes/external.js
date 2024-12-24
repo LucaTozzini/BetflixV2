c92ff01d20402214aaa26542879de1b6636fb832
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2024-12-24 02:58:41 -0800

    Update external.js

--- backend/routes/external.js
+++ backend/routes/external.js
@@ -5,7 +5,9 @@ const externalRouter = express.Router();
 externalRouter.get("/popular-movies", externalController.popularMovies);
 externalRouter.get("/movies", externalController.movies);
 externalRouter.get("/movies/:tmdbId", externalController.movieDetails);
+externalRouter.get("/movies/:tmdbId/images", externalController.movieImages);
 externalRouter.get("/shows", externalController.shows);
 externalRouter.get("/shows/:tmdbId", externalController.showDetails);
+externalRouter.get("/shows/:tmdbId/images", externalController.showImages);
 
 export default externalRouter;
