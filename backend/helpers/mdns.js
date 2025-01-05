c11578d4b7caa98c6a1312b0bdc9d627e40103a4
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2025-01-04 22:04:44 -0800

    mdns fix

--- backend/helpers/mdns.js
+++ backend/helpers/mdns.js
@@ -16,111 +16,131 @@ const MULTICAST_ADDRESS = "224.0.0.251";
 const MULTICAST_PORT = 5353;
 const TTL = 100; // in seconds
 
+// 00:15:5D is the start of Hyper-V MAC addresses (virtual machines)
+// https://macaddress.io/faq/how-to-recognise-a-microsoft-hyper-v-virtual-machine-by-its-mac-address
+
 const allInterf = os.networkInterfaces();
 const validInterf = [];
-for (const type in allInterf)
+for (const type in allInterf) {
   for (const interf of allInterf[type]) {
     // skip internal/virtual interfaces (VPNs)
-    if (interf.mac === "00:00:00:00:00:00" || interf.internal) continue;
+    if (
+      interf.mac === "00:00:00:00:00:00" ||
+      interf.mac.toLocaleLowerCase().startsWith("00:15:5d") ||
+      interf.internal
+    )
+      continue;
 
     // Only use IPv4 for now
     if (interf.family !== "IPv4") continue;
 
     validInterf.push({ address: interf.address, family: interf.family });
   }
-
-for (const interf of validInterf) {
-  const server = dgram.createSocket({ type: "udp4", reuseAddr: true });
-
-  // #region Answers
-  const answerPTR = {
-    name: "_http._tcp.local",
-    type: "PTR",
-    data: "Betflix.local",
-    class: "IN",
-    ttl: TTL,
-  };
-
-  const answerA = {
-    name: "Betflix.local",
-    type: "A",
-    data: interf.address, // Example IP address
-    class: "IN",
-    ttl: TTL,
-  };
-
-  const answerSRV = {
-    name: "Betflix.local",
-    type: "SRV",
-    data: {
-      port: process.env.SERVER_PORT,
-      target: "Betflix.local",
-    },
-    class: "IN",
-    ttl: TTL,
-  };
-
-  const answerTXT = {
-    name: "Betflix.local",
-    type: "TXT",
-    data: "Local Server for Betflix 2.0",
-    class: "IN",
-    ttl: TTL,
-  };
-  // #endregion
-
-  function respond(answers, port, address) {
-    const buf = dnsPacket.encode({
-      type: "response",
-      answers,
-    });
-
-    server.send(buf, 0, buf.length, port, address, (err, bytes) => {
-      if (err) console.error(err);
-    });
-  }
-
-  function handleMessage(msg, rinfo) {
-    try {
-      const packet = dnsPacket.decode(msg, 0);
-      if (packet.type === "query") {
-        const answers = [];
-        for (const question of packet.questions) {
-          if (question.name === "_http._tcp.local" && question.type === "PTR") {
-            answers.push(answerPTR);
-          } else if (
-            question.name === "Betflix.local" &&
-            question.type === "ANY"
-          ) {
-            answers.push(answerA);
-            answers.push(answerSRV);
-            answers.push(answerTXT);
-          }
-        }
-        if (answers.length) respond(answers, rinfo.port, rinfo.address);
-      }
-    } catch (err) {
-      // console.error("mdns handleMessage() error ->", err.message);
-    }
-  }
-
-  server.on("error", (err) => {
-    console.error(err.stack);
-    server.close();
-  });
-
-  server.on("message", handleMessage);
-
-  server.on("listening", () => {
-    const address = server.address();
-    console.log(`mDNS server listening @ ${address.address}:${address.port}`);
-    server.addMembership(MULTICAST_ADDRESS, interf.address);
-    respond(
-      [answerPTR, answerA, answerSRV, answerTXT],
-      MULTICAST_PORT,
-      MULTICAST_ADDRESS
-    );
-  });
-
-  server.bind(MULTICAST_PORT, interf.address);
 }
+
+if (!validInterf.length) throw new Error("No valid interfaces found");
+
+// #region Answers
+const answerPTR = {
+  name: "_http._tcp.local",
+  type: "PTR",
+  data: "Betflix.local",
+  class: "IN",
+  ttl: TTL,
+};
+
+// advertise all valid interfaces
+const answerA = validInterf.map((interf) => ({
+  name: "Betflix.local",
+  type: "A",
+  data: interf.address,
+  class: "IN",
+  ttl: TTL,
+}));
+
+const answerSRV = {
+  name: "Betflix.local",
+  type: "SRV",
+  data: {
+    port: process.env.SERVER_PORT,
+    target: "Betflix.local",
+  },
+  class: "IN",
+  ttl: TTL,
+};
+
+const answerTXT = {
+  name: "Betflix.local",
+  type: "TXT",
+  data: "Local Server for Betflix 2.0",
+  class: "IN",
+  ttl: TTL,
+};
+// #endregion
+
+const server = dgram.createSocket({ type: "udp4", reuseAddr: true });
+
+function respond(answers, port, address) {
+  const buf = dnsPacket.encode({
+    type: "response",
+    answers,
+  });
+
+  server.send(buf, 0, buf.length, port, address, (err, bytes) => {
+    if (err) console.error(err);
+  });
+}
+
+function handleMessage(msg, rinfo) {
+  try {
+    const packet = dnsPacket.decode(msg, 0);
+    if (packet.type === "query") {
+      const answers = [];
+      for (const question of packet.questions) {
+        if (question.name === "_http._tcp.local" && question.type === "PTR") {
+          answers.push(answerPTR);
+        } else if (
+          question.name === "Betflix.local" &&
+          question.type === "ANY"
+        ) {
+          answers.push(...answerA);
+          answers.push(answerSRV);
+          answers.push(answerTXT);
+        }
+      }
+      if (answers.length) respond(answers, rinfo.port, rinfo.address);
+    }
+  } catch (err) {
+    // console.error("mdns handleMessage() error ->", err.message);
+  }
+}
+
+server.on("error", (err) => {
+  console.error(err.stack);
+  server.close();
+});
+
+server.on("message", handleMessage);
+
+server.on("listening", () => {
+  // add first valid interface to multicast group
+  server.addMembership(MULTICAST_ADDRESS, validInterf[0].address);
+
+  // set outgoing interface to first valid interface
+  server.setMulticastInterface(validInterf[0].address);
+
+  console.log(
+    `mDNS server listening with ${validInterf[0].address}, advertising`,
+    validInterf.map((i) => i.address)
+  );
+
+  // send initial response
+  respond(
+    [answerPTR, ...answerA, answerSRV, answerTXT],
+    MULTICAST_PORT,
+    MULTICAST_ADDRESS
+  );
+});
+
+// listen for all traffic destined for the multicast port
+server.bind(MULTICAST_PORT);
